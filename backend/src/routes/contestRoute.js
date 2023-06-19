3b6f561c29205187bc4534f09f187ade0beb0498
Seon <seonpegado@gmail.com>
2023-06-20 00:27:45 +0530

    added contest creation and its functionalities

---
+++ backend/src/routes/contestRoute.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const {getContest,createContest,deleteContest,updateContest,addUser,removeUser} = require("../controllers/contest")
+const authentication = require("../middlewares/auth.js")
+
+const ContestRoutes = express();
+
+ContestRoutes.get('/', authentication , getContest)
+ContestRoutes.post('/', authentication , createContest)
+ContestRoutes.delete('/', authentication, deleteContest)
+ContestRoutes.put('/', authentication,updateContest)
+ContestRoutes.put('/add', authentication,addUser)
+ContestRoutes.put('/remove',authentication,removeUser)
+
+module.exports = {ContestRoutes}
